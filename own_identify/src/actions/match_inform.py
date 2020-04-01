f77287d369af4b863b394406843953144f98ff20
杨球松 <yangqiusong@cloudwalk.cn>
2020-04-01 10:04:03 +0800

    "update codes"

--- own_identify/src/actions/match_inform.py
+++ own_identify/src/actions/match_inform.py
@@ -10,8 +10,17 @@ _logger = logging.getLogger(__name__)
 
 
 class ActionMatchName(Action):
+    """匹配 own 姓名是否正确"""
     own_name = '杨球松'
+
+    def name(self):  # type: () -> Text
+        return 'action_match_name'
+
 
 
 class ActionMatchPassword(Action):
+    """匹配 own 设置密码是否匹配"""
     own_password = '20200202'
+
+    def name(self):  # type: () -> Text
+        return 'action_match_password'
--- own_identify/src/policies/self_rule_policy.py
+++ own_identify/src/policies/self_rule_policy.py
@@ -2,11 +2,49 @@
 # 根据场景编写的rule base policy module
 """
 import logging
+import json
 import os
+import typing
+from typing import Any, List, Text, Optional
+
+import rasa
+from rasa.core.domain import Domain, InvalidDomain
+from rasa.core.events import ActionExecuted
+from rasa.core.policies.policy import Policy
+from rasa.core.trackers import DialogueStateTracker
+from rasa.core.constants import MAPPING_POLICY_PRIORITY
 
 _logger = logging.getLogger(__name__)
 
 
-class SelfRulePolicy(object):
-    def __init__(self):
-        pass
+class SelfRulePolicy(Policy):
+    """self rule policy class"""
+    def __init__(self, priority: int = MAPPING_POLICY_PRIORITY) -> None:
+        """Create a new self rule policy."""
+
+        super().__init__(priority=priority)
+
+    def predict_action_probabilities(
+            self, tracker: DialogueStateTracker, domain: Domain
+    ) -> List[float]:
+        """Predicts the assigned action."""
+
+    def persist(self, path: Text) -> None:
+        """Only persists the priority."""
+
+        config_file = os.path.join(path, "self_rule_policy.json")
+        meta = {"priority": self.priority}
+        rasa.utils.io.create_directory_for_file(config_file)
+        rasa.utils.io.dump_obj_as_json_to_file(config_file, meta)
+
+    @classmethod
+    def load(cls, path: Text) -> "SelfRulePolicy":
+        """Returns the class with the configured priority."""
+
+        meta = {}
+        if os.path.exists(path):
+            meta_path = os.path.join(path, "self_rule_policy.json")
+            if os.path.isfile(meta_path):
+                meta = json.loads(rasa.utils.io.read_file(meta_path))
+
+        return cls(**meta)
